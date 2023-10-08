2eb933dd93191d146198450b9d4b25a561eb894d
Naeem-ur-rahman <naeemur1001@gmail.com>
2023-10-09 00:28:36 +0500

    Stream example

--- stream-learn/index.js
+++ stream-learn/index.js
@@ -1,13 +1,22 @@
 const express = require('express')
 const fs = require('fs')
+const status = require('express-status-monitor')
 
 const app = express();
 const PORT = 8000
 
+app.use(status())
+
+// app.get('/', (req, res) => {
+//     fs.readFile('./sample.txt', (_err, data) => {
+//         res.json({data});
+//     })
+// })
+
 app.get('/', (req, res) => {
-    fs.readFile('./sam.txt', (err, data) => {
-        res.end(data);
-    })
+    const stream = fs.createReadStream('./sample.txt', 'utf-8');
+    stream.on('data', (chunk) => res.write(chunk));
+    stream.on('end', () => res.end())
 })
 
-app.listen(PORT,()=>"Server Started")
+app.listen(PORT, () => `Server Started at PORT:${PORT}`)
